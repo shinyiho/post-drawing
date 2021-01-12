fe30fe2371e3e24f4be032cb8a52f2910016c78b
shinyiho <gbb2237@gmail.com>
2021-01-12 14:46:52 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+const upload_folder = "tmp";
+app.use(express.static("public"));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/uploaded", express.static(upload_folder));
+app.use("/assets", express.static("assets"));
+// // get list of posts
+app.get("/", (req, res) => {
+  res.send("sever running");
+});
+app.get("/posts", (req, res) => {
+  fs.promises.readdir(upload_folder).then((files) => {
+    res.send(JSON.stringify(files));
+  });
+});
+
+// Upload post route
+app.post("/upload", (req, res) => {
+  const { image } = req.body;
+
+  var base64Data = image.replace(/^data:image\/png;base64,/, "");
+
+  let id = Math.random().toFixed(8).toString().slice(2);
+
+  fs.writeFile(`${upload_folder}/${id}.png`, base64Data, "base64", (err) => console.log(err));
+
+  res.json({ id: id });
+  console.log("saved " + id);
+});
+
+// Server listener
+app.listen(3000, () => {
+  console.log("app is running");
+});
